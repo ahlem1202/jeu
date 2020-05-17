f733e19612f1f6d43c0080950507a9d180d45e70
ahlem1202 <61460599+ahlem1202@users.noreply.github.com>
2020-05-17 02:38:30 +0200

    saut
    avec doxygen

--- main.c
+++ main.c
@@ -1,54 +1,41 @@
+/**
+* @file main.c
+* @brief Testing Program
+* @author ahlem zaghouani
+* @version 0.1
+* @date May 2, 2020
+*
+* Testing program for saut
+*
+*/
 #include <stdio.h>
-#include <stdlib.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
-#include "SDL/SDL_getenv.h"
-//#include "SDL/SDL_mixer"
 #include"SDL/SDL_ttf.h"
-#include "joueur.h"
-
-int main(){
-	SDL_Surface *screen, *detec;
-	SDL_Event e;
-clavier in;
-SDL_Rect positiondetec;
-	screen = SDL_SetVideoMode(800,600,32,SDL_HWSURFACE);
-detec = IMG_Load("detective.png");
-	perso p;
-	perso_Init(&p);
-	
- do {
-        UpdateEvent(&in); // saisie clavier
-        if (in.key[SDLK_UP]){
-            positiondetec.y -= 1;
-        }
-        if (in.key[SDLK_DOWN]){
-            positiondetec.y += 1;
-        }
-        if (in.key[SDLK_RIGHT]){
-            positiondetec.x += 1;        }
-        if (in.key[SDLK_LEFT]){
-            positiondetec.x -= 1;
-        }
-          affichage(&p,screen);
-} while (!in.key[SDLK_ESCAPE]);
-  
-	int running = 1;
-
-	while(running){
-		while(SDL_PollEvent(&e)){
-			if(e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE){
-				running = 0;
-			}
-		}
-
-		SDL_FillRect(screen,NULL,0x000000);
-
-		perso_Render(&p,&screen);
-
-		SDL_Delay(25);
-		SDL_Flip(screen);
-	}
-SDL_FreeSurface(detec);	
-	SDL_Quit();
+#include"saut.h"
+int main(int argc,char** argv)
+{
+    SDL_Surface* screen;
+    int numkeys;
+    Uint8 * keys;
+    Sprite S;
+    Uint32 timer,elapsed;
+    SDL_Init(SDL_INIT_VIDEO);
+    screen=SDL_SetVideoMode(800,600,32,SDL_SWSURFACE|SDL_DOUBLEBUF);
+    InitSprite(&S);
+    do 
+    {
+        timer = SDL_GetTicks();
+        SDL_FillRect(screen,NULL,0);
+        SDL_PumpEvents();
+        keys = SDL_GetKeyState(&numkeys);
+        Evolue(&S,keys);
+        Render(&S,screen);
+        SDL_Flip(screen);
+        elapsed = SDL_GetTicks() - timer;
+        if (elapsed<20)
+            SDL_Delay(20-elapsed);
+    } while (!keys[SDLK_ESCAPE]);
+    SDL_Quit();
+    return 0;
 }
